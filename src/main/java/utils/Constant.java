eeaeaa438a82c114fa4d6dd0c29f8068d01c4c66
TimManas <timothy.romero.manas@gmail.com>
2021-01-09 15:19:02 -0500

    CreFirst commit

---
+++ src/main/java/utils/Constant.java
@@ -0,0 +1,15 @@
+package utils;
+
+public class Constant {
+
+	/**Config Properties file **/	
+	public final static String CONFIG_PROPERTIES_DIRECTORY = "properties//config.properties";
+	
+	public final static String GECKO_DRIVER_DIRECTORY = System.getProperty("user.dir") + "//src\\test\\java\\resources\\other\\geckodriver";
+	
+	public final static String CHROME_DRIVER_DIRECTORY = System.getProperty("user.dir") + "//src//test//java//resources//other//chromedriver";
+	
+	public final static String IE_DRIVER_DIRECTORY = System.getProperty("user.dir") + "\\src\\test\\java\\resources\\other\\IEDriverServer";
+
+
+}
---
+++ target/cucumber/report.js
@@ -0,0 +1,254 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("contactus.feature");
+formatter.feature({
+  "line": 1,
+  "name": "Submit data to webdriveruniversity.com using contact us form",
+  "description": "",
+  "id": "submit-data-to-webdriveruniversity.com-using-contact-us-form",
+  "keyword": "Feature"
+});
+formatter.before({
+  "duration": 2609089988,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 3,
+  "name": "Submit valid data via contact us form",
+  "description": "",
+  "id": "submit-data-to-webdriveruniversity.com-using-contact-us-form;submit-valid-data-via-contact-us-form",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 4,
+  "name": "I access webdriveruniversity contact us form",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 5,
+  "name": "I enter a valid firstname",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 6,
+  "name": "I enter a valid last name",
+  "rows": [
+    {
+      "cells": [
+        "woods",
+        "jackson",
+        "jones"
+      ],
+      "line": 7
+    }
+  ],
+  "keyword": "When "
+});
+formatter.step({
+  "line": 8,
+  "name": "i enter a valid email address",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 9,
+  "name": "i enter comments",
+  "rows": [
+    {
+      "cells": [
+        "example comment one",
+        "example comment two"
+      ],
+      "line": 10
+    }
+  ],
+  "keyword": "And "
+});
+formatter.step({
+  "line": 11,
+  "name": "i click on the submit button",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 12,
+  "name": "the information should successfully be submitted via the contact us form",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "ContactUsSteps.i_access_webdriveruniversity_contact_us_form()"
+});
+formatter.result({
+  "duration": 1219439686,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ContactUsSteps.i_enter_a_valid_firstname()"
+});
+formatter.result({
+  "duration": 501840969,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ContactUsSteps.i_enter_a_valid_last_name(DataTable)"
+});
+formatter.result({
+  "duration": 188270207,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ContactUsSteps.i_enter_a_valid_email_address()"
+});
+formatter.result({
+  "duration": 209058963,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ContactUsSteps.i_enter_comments(DataTable)"
+});
+formatter.result({
+  "duration": 160103340,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ContactUsSteps.i_click_on_the_submit_button()"
+});
+formatter.result({
+  "duration": 1201188995,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ContactUsSteps.the_information_should_successfully_be_submitted_via_the_contact_us_form()"
+});
+formatter.result({
+  "duration": 47639103,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 175785767,
+  "status": "passed"
+});
+formatter.uri("products.feature");
+formatter.feature({
+  "line": 1,
+  "name": "Products page",
+  "description": "",
+  "id": "products-page",
+  "keyword": "Feature"
+});
+formatter.scenarioOutline({
+  "line": 3,
+  "name": "validate promo code alert is visible when clicking on the special offers link",
+  "description": "",
+  "id": "products-page;validate-promo-code-alert-is-visible-when-clicking-on-the-special-offers-link",
+  "type": "scenario_outline",
+  "keyword": "Scenario Outline"
+});
+formatter.step({
+  "line": 4,
+  "name": "user navigates to \"\u003curl\u003e\" website",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 5,
+  "name": "user clicks on \"\u003cbutton\u003e\"",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 6,
+  "name": "user should be presented with a promo alert",
+  "keyword": "Then "
+});
+formatter.examples({
+  "line": 8,
+  "name": "",
+  "description": "",
+  "id": "products-page;validate-promo-code-alert-is-visible-when-clicking-on-the-special-offers-link;",
+  "rows": [
+    {
+      "cells": [
+        "url",
+        "button"
+      ],
+      "line": 9,
+      "id": "products-page;validate-promo-code-alert-is-visible-when-clicking-on-the-special-offers-link;;1"
+    },
+    {
+      "cells": [
+        "http://www.webdriveruniversity.com/Page-Object-Model/products.html",
+        "#container-special-offers"
+      ],
+      "line": 10,
+      "id": "products-page;validate-promo-code-alert-is-visible-when-clicking-on-the-special-offers-link;;2"
+    }
+  ],
+  "keyword": "Examples"
+});
+formatter.before({
+  "duration": 1659238202,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 10,
+  "name": "validate promo code alert is visible when clicking on the special offers link",
+  "description": "",
+  "id": "products-page;validate-promo-code-alert-is-visible-when-clicking-on-the-special-offers-link;;2",
+  "type": "scenario",
+  "keyword": "Scenario Outline"
+});
+formatter.step({
+  "line": 4,
+  "name": "user navigates to \"http://www.webdriveruniversity.com/Page-Object-Model/products.html\" website",
+  "matchedColumns": [
+    0
+  ],
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 5,
+  "name": "user clicks on \"#container-special-offers\"",
+  "matchedColumns": [
+    1
+  ],
+  "keyword": "When "
+});
+formatter.step({
+  "line": 6,
+  "name": "user should be presented with a promo alert",
+  "keyword": "Then "
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "http://www.webdriveruniversity.com/Page-Object-Model/products.html",
+      "offset": 19
+    }
+  ],
+  "location": "ProductSteps.user_navigates_to_website(String)"
+});
+formatter.result({
+  "duration": 1757368893,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "#container-special-offers",
+      "offset": 16
+    }
+  ],
+  "location": "ProductSteps.user_clicks_on(String)"
+});
+formatter.result({
+  "duration": 1472803213,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ProductSteps.user_should_be_presented_with_a_promo_alert()"
+});
+formatter.result({
+  "duration": 248671176,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 179843415,
+  "status": "passed"
+});
+});
